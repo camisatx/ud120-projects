5ac018f0a2dd8d156197e4df138690db331152f2
camisatx <joshschertz3@gmail.com>
2015-05-12 23:05:07 -0500

    Added code for a Naive Bayes Classifier that trains and predicts the person who sent a particular email.

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -15,20 +15,24 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
-
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = GaussianNB()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training took %.2f seconds" % float(time() - t0))
 
+t1 = time()
+pred = clf.predict(features_test)
+print("Predicting took %.2f seconds" % float(time() - t1))
 
-#########################################################
-### your code goes here ###
+acc = accuracy_score(pred, labels_test)
 
-
-#########################################################
-
-
+print("Prediction accuracy is %.2f percent" % float(acc * 100))
