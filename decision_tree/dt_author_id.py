71af7da29d050f6c32d82f7c0b9ad9e683a81695
camisatx <joshschertz3@gmail.com>
2015-05-15 00:43:22 -0500

    Completed Decision Tree (Lesson 3)
    Finished adding code to implement decision tree classification.

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,22 +12,26 @@
     
 import sys
 from time import time
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print('Training took %.2f seconds' % float(time() - t0))
 
+t1 = time()
+pred = clf.predict(features_test)
+print('Predicting took %.2f seconds' % float(time() - t1))
 
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+acc = accuracy_score(pred, labels_test)
+print('The accuracy was %.2f percent' % float(acc * 100))
